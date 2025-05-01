a8d953f88a500d29617a608f77bfc417d16b4dd2
yehonatan169 <76455181+yehonatan169@users.noreply.github.com>
2025-05-01 22:19:53 +0300

    fix some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import StreamPage from "./pages/StreamPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind עובד 🎉
-      </h1>
+    <Router>
+      <div className="p-4">
+        <nav className="flex gap-4 mb-6">
+          <Link to="/" className="text-blue-600 hover:underline">Stream</Link>
+          <Link to="/dashboard" className="text-blue-600 hover:underline">Dashboard</Link>
+        </nav>
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Routes>
+          <Route path="/" element={<StreamPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
